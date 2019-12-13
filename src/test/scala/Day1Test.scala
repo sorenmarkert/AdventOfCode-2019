fcfc65010d043b1e363012f7a681609d7ee331d8
Søren Markert <smarkert@gmail.com>
2019-12-13 10:46:30 +0100

    Day1 - Updated

--- src/test/scala/Day1Test.scala
+++ src/test/scala/Day1Test.scala
@@ -1,12 +1,20 @@
-import org.scalatest.GivenWhenThen
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.must.Matchers._
+import Day1._
 
-class Day1Test extends AnyFlatSpec with GivenWhenThen {
+class Day1Test extends AnyFlatSpec {
 
-    "A thing" should "return the correct result" in {
+    "The fuel calculator" should "calculate the correct fuel" in {
+        calculateFuel(12 :: Nil) mustEqual 2
+        calculateFuel(14 :: Nil) mustEqual 2
+        calculateFuel(1969 :: Nil) mustEqual 654
+        calculateFuel(100756 :: Nil) mustEqual 33583
+    }
 
-        Given("something")
-        val v = 1
-        assert(v == 1)
+    "The total fuel calculator" should "calculate the correct fuel" in {
+        calculateTotalFuel(12 :: Nil) mustEqual 2
+        calculateTotalFuel(14 :: Nil) mustEqual 2
+        calculateTotalFuel(1969 :: Nil) mustEqual 966
+        calculateTotalFuel(100756 :: Nil) mustEqual 50346
     }
 }
---
+++ src/test/scala/Day2Test.scala
@@ -0,0 +1,15 @@
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.must.Matchers._
+import Day2._
+
+class Day2Test extends AnyFlatSpec {
+
+  "The Intcode computer" should "return the correct results" in {
+
+    processOpCodes(Array(1,9,10,3,2,3,11,0,99,30,40,50))(0) mustEqual 3500
+    processOpCodes(Array(1,0,0,0,99))(0) mustEqual 2
+    processOpCodes(Array(2,3,0,3,99))(3) mustEqual 6
+    processOpCodes(Array(2,4,4,5,99,0))(5) mustEqual 9801
+    processOpCodes(Array(1,1,1,4,99,5,6,0,99))(0) mustEqual 30
+  }
+}
