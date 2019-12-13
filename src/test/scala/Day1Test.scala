52de14610285dc36f033c2de3098903233deb68e
Søren Markert <smarkert@gmail.com>
2019-12-13 10:46:30 +0100

    Day1 - Updated

--- src/test/scala/Day1Test.scala
+++ src/test/scala/Day1Test.scala
@@ -1,12 +1,13 @@
-import org.scalatest.GivenWhenThen
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.must.Matchers._
+import Day1._
 
-class Day1Test extends AnyFlatSpec with GivenWhenThen {
+class Day1Test extends AnyFlatSpec {
 
-    "A thing" should "return the correct result" in {
-
-        Given("something")
-        val v = 1
-        assert(v == 1)
+    "The fuel calculator" should "calculate the correct fuel" in {
+        calculateFuel(12 :: Nil) mustEqual 2
+        calculateFuel(14 :: Nil) mustEqual 2
+        calculateFuel(1969 :: Nil) mustEqual 654
+        calculateFuel(100756 :: Nil) mustEqual 33583
     }
 }
