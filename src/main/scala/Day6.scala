6f6ef3c640b44e2a72ecfa2204592ed8962ddc5b
Søren Markert <smarkert@gmail.com>
2019-12-14 17:18:22 +0100

    Day 6 - Universal Orbit Map - Part 2

--- src/main/scala/Day6.scala
+++ src/main/scala/Day6.scala
@@ -1,4 +1,3 @@
-import java.io.{File, PrintWriter}
 
 import scala.io.Source
 
@@ -6,17 +5,27 @@ object Day6 {
 
     def main(args: Array[String]): Unit = {
 
-        val input = Source.fromFile("input/day6.txt").getLines
+        val input = Source.fromFile("input/day6.txt").getLines.toList
         println(orbitCount(input))
+        println(orbitalTransfers(input))
     }
 
-    def orbitCount(mapStrings: Iterator[String]) = {
+    def orbitalTransfers(mapStrings: List[String]) = {
 
-        def parseMap(mapString: Iterator[String]): Map[String, String] =
-            mapString.map { line =>
-                val parts = line.split("\\)")
-                parts(1) -> parts(0)
-            }.toMap
+        def getOrbitalLine(obj: String, map: Map[String, String]): List[String] = map(obj) match {
+            case "COM" => "COM" :: Nil
+            case x => x :: getOrbitalLine(x, map)
+        }
+
+        val map = parseMap(mapStrings)
+        val santa = getOrbitalLine("SAN", map).reverse
+        val you = getOrbitalLine("YOU", map).reverse
+
+        val sharedOrbitLength = you.zip(santa).filter(x => x._1 == x._2).length
+        you.length + santa.length - (2 * sharedOrbitLength)
+    }
+
+    def orbitCount(mapStrings: List[String]) = {
 
         def countOrbits(obj: String, map: Map[String, String]): Int = obj match {
             case "COM" => 0
@@ -26,4 +35,10 @@ object Day6 {
         val map = parseMap(mapStrings)
         map.map{ case (k, _) => countOrbits(k, map) }.sum
     }
+
+    private def parseMap(mapString: List[String]) =
+        mapString.map { line =>
+            val parts = line.split("\\)")
+            parts(1) -> parts(0)
+        }.toMap
 }
--- src/test/scala/Day6Test.scala
+++ src/test/scala/Day6Test.scala
@@ -16,8 +16,27 @@ class Day6Test extends AnyFlatSpec {
           |D)I
           |E)J
           |J)K
-          |K)L""".stripMargin.linesIterator
+          |K)L""".stripMargin.linesIterator.toList
 
         orbitCount(testMap) mustEqual 42
+    }
+
+    "The map checksum calculator" should "calculate the correct number of orbital transfers" in {
+
+        val testMap = """COM)B
+                        |B)C
+                        |C)D
+                        |D)E
+                        |E)F
+                        |B)G
+                        |G)H
+                        |D)I
+                        |E)J
+                        |J)K
+                        |K)L
+                        |K)YOU
+                        |I)SAN""".stripMargin.linesIterator.toList
+
+        orbitalTransfers(testMap) mustEqual 4
     }
 }
