6855712f0c8b3f10bc2bee8b2af39866275760a7
Søren Markert <smarkert@gmail.com>
2019-12-15 02:11:49 +0100

    Day 7 - Amplification Circuit - Part 2

--- src/main/scala/Day7.scala
+++ src/main/scala/Day7.scala
@@ -1,6 +1,7 @@
 import Day5.runProgram
 
-import scala.collection.mutable.{ Set => MSet }
+import scala.collection.mutable.{Queue => MQueue}
+import scala.collection.mutable.{Set => MSet}
 
 object Day7 {
 
@@ -11,9 +12,13 @@ object Day7 {
         val permutations = MSet.empty[List[Int]]
         permute(permutations)
         println(permutations.map(phaseSettings => amplifySignal(phaseSettings, puzzleInput)).max)
+
+        val feedbackPermutations = MSet.empty[List[Int]]
+        permute(feedbackPermutations, (5 to 9).toList)
+        println(feedbackPermutations.map(phaseSettings => feedbackAmplify(phaseSettings, puzzleInput)).max)
     }
 
-    def permute(results: MSet[List[Int]], phaseSettings: List[Int] = List(0, 1, 2, 3, 4), result: List[Int] = Nil): Unit =
+    def permute(results: MSet[List[Int]], phaseSettings: List[Int] = (0 to 4).toList, result: List[Int] = Nil): Unit =
         phaseSettings match {
             case Nil => results += result
             case _ => phaseSettings.foreach(x => permute(results, phaseSettings diff List(x), x :: result))
@@ -21,13 +26,103 @@ object Day7 {
 
     def amplifySignal(phaseSettings: List[Int], program: List[Int]) = {
 
-        def runIt(phaseSetting: Int, input: Int) = runProgram(program.toArray, Iterator(phaseSetting, input))
+        def runAmplifier(input: Int, amplifier: Int) =
+            runProgram(program.toArray, Iterator(phaseSettings(amplifier), input)).head
 
-        val result1 = runIt(phaseSettings(0), 0)
-        val result2 = runIt(phaseSettings(1), result1.head)
-        val result3 = runIt(phaseSettings(2), result2.head)
-        val result4 = runIt(phaseSettings(3), result3.head)
-        val result5 = runIt(phaseSettings(4), result4.head)
-        result5.head
+        def runAmplifiers(input: Int = 0, amplifier: Int = 0): Int =
+            if (amplifier >= 4) runAmplifier(input, amplifier)
+            else runAmplifiers(runAmplifier(input, amplifier), amplifier + 1)
+
+        runAmplifiers()
     }
+
+    def feedbackAmplify(phaseSettings: List[Int], program: List[Int]) = {
+
+        val runningAmplifiers = phaseSettings
+            .map(x => PausedProgram(program.toArray, 0, MQueue(x), null, false))
+            .toArray
+
+        def restartPrograms(amplifier: Int, inputs: MQueue[Int]): MQueue[Int] = {
+            val x = runningAmplifiers(amplifier)
+            if (x.isCompleted) {
+                runningAmplifiers(4).outputs
+            }
+            else {
+                val res = runWirableProgram(x.program, x.pointer, x.inputs appendAll inputs)
+                runningAmplifiers.update(amplifier, res)
+                restartPrograms((amplifier + 1) % 5, res.outputs)
+            }
+        }
+
+        restartPrograms(0, MQueue(0)).dequeue
+    }
+
+    def runWirableProgram(program: Array[Int], pointer: Int, inputs: MQueue[Int]) = {
+
+        val outputs = MQueue.empty[Int]
+
+        def processOpCodes(pointer: Int): PausedProgram = {
+
+            def getParameter(position: Int, mode: Char) = mode match {
+                case '0' => program(program(position))
+                case '1' => program(position)
+                case _ => throw new IllegalStateException("Unknown parameter mode " + mode + " at " + program(pointer))
+            }
+
+            val (opCode, modes) = {
+                val opCodeString = "000" + program(pointer).toString
+                (opCodeString.takeRight(2).toInt, opCodeString.reverse.drop(2))
+            }
+
+            opCode match {
+                case 1 => { // addition
+                    program.update(program(pointer + 3), getParameter(pointer + 1, modes(0)) + getParameter(pointer + 2, modes(1)))
+                    processOpCodes(pointer + 4)
+                }
+                case 2 => { // multiplication
+                    program.update(program(pointer + 3), getParameter(pointer + 1, modes(0)) * getParameter(pointer + 2, modes(1)))
+                    processOpCodes(pointer + 4)
+                }
+                case 3 => { // input
+                    if (inputs.isEmpty) {
+                        PausedProgram(program, pointer, inputs, outputs, false)
+                    } else {
+                        program.update(program(pointer + 1), inputs.dequeue)
+                        processOpCodes(pointer + 2)
+                    }
+                }
+                case 4 => { // output
+                    outputs.enqueue(getParameter(pointer + 1, modes(0)))
+                    processOpCodes(pointer + 2)
+                }
+                case 5 => { // jump if true
+                    processOpCodes(
+                        if (0 != getParameter(pointer + 1, modes(0))) getParameter(pointer + 2, modes(1))
+                        else pointer + 3)
+                }
+                case 6 => { // jump if false
+                    processOpCodes(
+                        if (0 == getParameter(pointer + 1, modes(0))) getParameter(pointer + 2, modes(1))
+                        else pointer + 3)
+                }
+                case 7 => { // less than
+                    program.update(program(pointer + 3),
+                        if (getParameter(pointer + 1, modes(0)) < getParameter(pointer + 2, modes(1))) 1 else 0)
+                    processOpCodes(pointer + 4)
+                }
+                case 8 => { // equals
+                    program.update(program(pointer + 3),
+                        if (getParameter(pointer + 1, modes(0)) == getParameter(pointer + 2, modes(1))) 1 else 0)
+                    processOpCodes(pointer + 4)
+                }
+                case 99 => PausedProgram(program, pointer, inputs, outputs, true)
+                case _ => throw new IllegalStateException("Unknown OpCode at " + pointer + ":" + program(pointer))
+            }
+        }
+
+        processOpCodes(pointer)
+    }
+
+    case class PausedProgram(program: Array[Int], pointer: Int, inputs: MQueue[Int], outputs: MQueue[Int], isCompleted: Boolean)
+
 }
--- src/test/scala/Day7Test.scala
+++ src/test/scala/Day7Test.scala
@@ -6,13 +6,34 @@ class Day7Test extends AnyFlatSpec {
 
     "The amplifier software" should "calculate the correct output" in {
 
-        val amplifierControllerSoftware1 = List(3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0)
-        amplifySignal(List(4,3,2,1,0), amplifierControllerSoftware1) mustEqual 43210
+        val amplifierControllerSoftware1 = List(3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0)
+        amplifySignal(List(4, 3, 2, 1, 0), amplifierControllerSoftware1) mustEqual 43210
 
-        val amplifierControllerSoftware2 = List(3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0)
-        amplifySignal(List(0,1,2,3,4), amplifierControllerSoftware2) mustEqual 54321
+        val amplifierControllerSoftware2 = List(3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23, 101, 5, 23, 23, 1, 24, 23, 23, 4, 23, 99, 0, 0)
+        amplifySignal(List(0, 1, 2, 3, 4), amplifierControllerSoftware2) mustEqual 54321
 
-        val amplifierControllerSoftware3 = List(3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0)
-        amplifySignal(List(1,0,4,3,2), amplifierControllerSoftware3) mustEqual 65210
+        val amplifierControllerSoftware3 = List(3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33, 1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0)
+        amplifySignal(List(1, 0, 4, 3, 2), amplifierControllerSoftware3) mustEqual 65210
+    }
+
+    "The feedback amplifier" should "calculate the correct output" in {
+
+        val amplifierControllerSoftware1 = List(3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0)
+        feedbackAmplify(List(4, 3, 2, 1, 0), amplifierControllerSoftware1) mustEqual 43210
+
+        val amplifierControllerSoftware2 = List(3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23, 101, 5, 23, 23, 1, 24, 23, 23, 4, 23, 99, 0, 0)
+        feedbackAmplify(List(0, 1, 2, 3, 4), amplifierControllerSoftware2) mustEqual 54321
+
+        val amplifierControllerSoftware3 = List(3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33, 1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0)
+        feedbackAmplify(List(1, 0, 4, 3, 2), amplifierControllerSoftware3) mustEqual 65210
+    }
+
+    "The feedback amplifiers" should "calculate the correct output" in {
+
+        val amplifierControllerSoftware1 = List(3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1, 27, 26, 27, 4, 27, 1001, 28, -1, 28, 1005, 28, 6, 99, 0, 0, 5)
+        feedbackAmplify(List(9, 8, 7, 6, 5), amplifierControllerSoftware1) mustEqual 139629729
+
+        val amplifierControllerSoftware2 = List(3, 52, 1001, 52, -5, 52, 3, 53, 1, 52, 56, 54, 1007, 54, 5, 55, 1005, 55, 26, 1001, 54, -5, 54, 1105, 1, 12, 1, 53, 54, 53, 1008, 54, 0, 55, 1001, 55, 1, 55, 2, 53, 55, 53, 4, 53, 1001, 56, -1, 56, 1005, 56, 6, 99, 0, 0, 0, 0, 10)
+        feedbackAmplify(List(9, 7, 8, 5, 6), amplifierControllerSoftware2) mustEqual 18216
     }
 }
