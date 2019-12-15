f45885d8c7ac03d8cf580ba5d18d56f8f461f4fc
Søren Markert <smarkert@gmail.com>
2019-12-15 10:47:58 +0100

    Day 8 - Space Image Format - Part 2

--- src/main/scala/Day8.scala
+++ src/main/scala/Day8.scala
@@ -3,16 +3,19 @@ import scala.io.Source
 
 object Day8 {
 
-    val imageSize = 25 * 6
+    val imageWidth = 25
+    val imageHeight = 6
+    val imageSize = imageWidth * imageHeight
 
     def main(args: Array[String]): Unit = {
 
-        val inputString = Source.fromFile("input/day8.txt").getLines.next
+        val layers = parseImage(Source.fromFile("input/day8.txt").getLines.next)
 
-        val layers = parseImage(inputString)
+        val checksumLayer = findFewestZeros(layers)
+        println(checksum(layers(checksumLayer)))
 
-        val layer = findFewestZeros(layers)
-        println(checksum(layers(layer)))
+        val image = decodeImage(layers)
+        println(image)
     }
 
     def parseImage(input: String) = input.grouped(imageSize).toVector
@@ -23,4 +26,19 @@ object Day8 {
         ._2
 
     def checksum(layer: String) = layer.count(_ == '1') * layer.count(_ == '2')
+
+    def decodeImage(layers: Vector[String]) = (for {
+        position <- 0 until imageSize
+        layer <- 0 until layers.size
+    } yield {
+        layers(layer)(position)
+    })
+        .grouped(layers.size)
+        .map(_.find(_ != '2'))
+        .flatten
+        .map(c => Map('0' -> ' ', '1' -> '#')(c))
+        .grouped(imageWidth)
+        .map(_.mkString)
+        .mkString("\n")
+
 }
